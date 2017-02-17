9bbc2ceae7a421031d96931021c47f70c25f641a
Ira <eireen.zhilyaeva.97@gmail.com>
2017-02-17 18:14:27 +0500

    Edge and Point

---
+++ Lab1Triangles/Edge.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1Triangles
+{
+    class Edge
+    {
+        public Point P1; //1-я точка ребра
+        public Point P2; //2-я точка ребра
+
+        public Edge(Point p1, Point p2)
+        {
+            this.P1 = p1;
+            this.P2 = p2;
+        }
+
+        public double Length//длина ребра
+        {
+            get
+            {
+                return Math.Sqrt(Math.Pow((P2.X - P1.X), 2) + Math.Pow((P2.Y - P1.Y), 2));
+            }
+        }
+
+    }
+}
